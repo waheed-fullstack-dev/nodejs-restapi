29e6b6c1d8bc01e82c5ce41a5cb71f7c8caff94f
Mohammad Waheed <itsswaheed@gmail.com>
2022-12-29 22:24:43 +0500

    initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+		jest: true
+	},
+	extends: 'eslint:recommended',
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		indent: ['error', 4],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'double'],
+		semi: ['error', 'always'],
+		'no-empty': 'warn',
+		'no-cond-assign': ['error', 'always'],
+		'for-direction': 'off',
+	},
+
+	settings: {
+		'import/parser': 'babel-eslint',
+		sharedData: 'Hello',
+	},
+};
